4084d4b455892b3441130c40c384dc0345f77b1f
Sanderson Ferraz <sandersonferraz@gmail.com>
2025-07-21 22:15:48 -0300

    feat: Add credito API

---
+++ credito-api/src/main/java/com/desafio/credito_api/CreditoApiApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.credito_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditoApiApplication.class, args);
+	}
+
+}
